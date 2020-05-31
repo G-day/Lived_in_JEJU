9a5e19fa3a4648e7bb1260350746fa98b39a8c90
yeong <qoskdud15@daum.net>
2020-05-31 16:59:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/gday_project/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.gday_project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+    }
+
+
+    // 회원가입 버튼 클릭 시
+    public void joinBtn(View v){
+
+    }
+}
