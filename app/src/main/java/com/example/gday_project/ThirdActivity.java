4d1de3aa82b5d30e5278f146f7ea860ca3870aac
chasonghui <chasonghui@naver.com>
2020-06-10 21:31:10 +0900

    구글맵_ver3.3     - 마커 커스텀-사진추가

--- app/src/main/java/com/example/gday_project/ThirdActivity.java
+++ app/src/main/java/com/example/gday_project/ThirdActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -41,13 +42,14 @@ public class ThirdActivity extends AppCompatActivity implements OnMapReadyCallba
         markerOptions.position(new LatLng(33.389713, 126.235304))//위도 경도(고살리숲길)위치설정
                 .title("금능으뜸원해변")
                 .snippet("제주도 산책로 : 해변")
-                .alpha(0.7f);//마커의 투명도 설정
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.human));//마커이미지변경
         mMap.addMarker(markerOptions);//마커 생성
 
         markerOptions.position(new LatLng(33.385831, 126.227402))//
                 .title("금능 식물원")
                 .snippet("제주도 산책로 : 식물원")
-                .alpha(0.7f);//마커의 투명도 설정
+                .alpha(0.7f)//마커의 투명도 설정
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.human));//마커이미지변경
 
         mMap.addMarker(markerOptions);//마커 생성
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(33.387026, 126.231737), 14));
