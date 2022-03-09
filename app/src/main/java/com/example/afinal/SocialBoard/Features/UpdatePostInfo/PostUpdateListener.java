461b61174f20f41993f1cd3b2056881bb2480e54
차송희 <chasonghui@naver.com>
2022-03-09 22:22:53 +0900

    final

--- app/src/main/java/com/example/afinal/SocialBoard/Features/UpdatePostInfo/PostUpdateListener.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2020. 문재식 All rights reserved //
-
-
-package com.example.afinal.SocialBoard.Features.UpdatePostInfo;
-import com.example.afinal.SocialBoard.Features.CreatePost.Post;
-
-public interface PostUpdateListener {
-    void onPostInfoUpdated(Post post, int position);
-}
